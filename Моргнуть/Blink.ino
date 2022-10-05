3ff5aca2e154b0b766e05dc37f88ad301866f8b3
Plyusnina Sofya <114556853+plyussofya@users.noreply.github.com>
2022-10-05 23:40:59 +0800

    Update and rename Blink/Blink.ino to Моргнуть/Blink.ino

--- Моргнуть/Blink.ino
+++ Моргнуть/Blink.ino
@@ -1,6 +1,9 @@
 #define led_pin 3
 #define sensor_pin A2
 
+int max = 0;
+int min = 1024;
+
 void setup() {
   Serial.begin(9600);
   pinMode(led_pin, OUTPUT);
@@ -8,7 +11,14 @@ void setup() {
 void loop() {
   int val = analogRead(sensor_pin);
   Serial.println(val);
-  if (val < 840){
+  if (max < val)
+    max = val;
+  if (min > val)
+    min = val;
+  Serial.println(max);
+  Serial.println(min);
+  
+  if ((max + min)/2 > val){
       digitalWrite(led_pin, LOW);    
     }
    else {
